12fb0ce264369d045108a864985317e5a7e5d21d
ahsaanudeen <ahsaanuddin7@gmail.com>
2025-06-02 12:35:26 +0530

    commit

---
+++ CRM_Project/src/test/java/com/CRM_Project/CRM_Project/CrmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRM_Project.CRM_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
